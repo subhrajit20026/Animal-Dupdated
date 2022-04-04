1de294822daec45a6e9db4f05cd8bd4559ac4e7c
Subhrajit Kumar Das <76467245+DesmondSk26@users.noreply.github.com>
2022-04-04 15:49:05 +0530

    Add files via upload

---
+++ Animal.py
@@ -0,0 +1,415 @@
+print("::Animal Identifying ::")
+n=int(input("Everytime\nPress 1 for yes\nPress 2 for NO: \n"))
+print("Is Your Animal is Wild or Domestic?\nWild Press 1\nDomestic Press 2")
+n=int(input())
+#Domestic Birds
+if n==2:
+    print("How Many Legs It have??")
+    n=int(input())
+    if n==2:
+        n=int(input("Can it Fly?\n"))
+        if n==1:
+            print("Its a Bird")
+        else:
+            print("Can we eat It??")
+            n=int(input())
+            if n==1:
+                print("Do This Bird chirps in morning?")
+                n=int(input())
+                if n==1:
+                    print("Its Hen!!")
+                else:
+                    print("Can It Fly??")
+                    n=int(input())
+                    if n==1:
+                        print("Its a Pigeon!")
+                    else:
+                        print("Its A Duck!!")
+            else:
+                print("Is It a Night Bird??")
+                n=int(input())
+                if n==1:
+                    print("Its An OwL!!")
+#Domestic Mamals
+    else:
+        print("Dose It stay in inside Home?")
+        n=int(input())
+        if n==1:
+            print("Does it eat grass??")
+            n=int(input())
+            if n==1:
+                print("Its A Rabbit")
+            else:
+                print("Can it run and jump fast??")
+                n=int(input())
+                if n==1:
+                    print("Its a Cat")
+                else:
+                    print("It is a Doggo")
+        else:
+            print("Can we eat It?")
+            n=int(input())
+            if n==1:
+                print("Do Its Fur is Important to us??")
+                n=int(input())
+                if n==1:
+                    print("I think Its A Sheep!")
+                else:
+                    print("Has It Hair in Body??")
+                    n=int(input())
+                    if n==1:
+                        print("You Think a Goat!")
+                    else:
+                        print("Its A Pig!!")
+            
+            else:
+                print("Can It run fast??")
+                n=int(input())
+                if n==1:
+                    print("Is it Small?")
+                    n=int(input())
+                    if n==1:
+                        print("Its a Rabbit!!")
+                    else:
+                        print("Its a Horse")
+                else:
+                    print("Is It stay in Farm??")
+                    n=int(input())
+                    if n==1:
+                        print("It Gives Milk?? :)")
+                        n=int(input())
+                        if n==1:
+                            print("Its a Desert animal?")
+                            n=int(input())
+                            if n==1:
+                                print("It a Camel")
+                            else:
+                                print("Is It Strong and run fast?")
+                                n=int(input())
+                                if n==1:
+                                    print("Its a OX")
+                                else:
+                                    print("It is a Cow")
+                        else:
+                            print("Do Human use them for carrying Load?")
+                            n=int(input())
+                            if n==1:
+                                print("I think Its A Donkey!!")
+#Wild
+else:
+#flying ones
+    n=int(input("Can it Fly?\n"))
+    if n==1:
+        print("It has a Long Vision and Fly high?")
+        n=int(input())
+        if n==1:
+            print("Its A Eagle!!")
+        else:
+            print("Does It sleep upside down??")
+            n=int(input())
+            if n==1:
+                print("Its A Bat I think")
+#Sea Mamals
+    else:
+        print("Is it a Sea Animal??")
+        n=int(input())
+        if n==1:
+            print("Is it too big compared to a Dolphin?")
+            n=int(input())
+            if n==1:
+                print("Its a Whale!!")
+            else:
+                n=int(input("Is it has 8 hands??\n"))
+                if n==1:
+                    n=int(input("Is it a Intelligent animal??\n"))
+                    if n==1:
+                        print("Its a Octopus")
+                    else:
+                        print("Its A Squid!!")
+                else:
+                    print("Does It has Fins??")
+                    n=int(input())
+                    if n==1:
+                        print("Is it generally dangerous for Human?? ")
+                        n=int(input())
+                        if n==1:
+                            print("Its a Shark")
+                        else:
+                            print("I think its a Dolphin!!")
+                    else:
+                        print("Is it poisonous and transparent body??")
+                        n=int(input())
+                        if n==1:
+                            print("Its a Jelly Fish")
+                        else:
+                            print("Does it looks like star??")
+                            n=int(input())
+                            if n==1:
+                                print("Its a Star-Fish")
+                            else:
+                                print("Is it walk too slow compared to human??")
+                                n=int(input())
+                                if n==1:
+                                    print("Is it small in size??")
+                                    n=int(input())
+                                    if n==1:
+                                        print("I think its a Snail!!")
+                                    else:
+                                        print("It has 4 lEGS??")
+                                        n=int(input())
+                                        if n==1:
+                                            print("Its A Turtle!!")
+                                        else:
+                                           print("Its a Seal!!") 
+                                        
+                                else:
+                                    print("Does it live in cold countries?")
+                                    n=int(input())
+                                    if n==1:
+                                        print("Does Its has 4 legs??")
+                                        n=int(input())
+                                        if n==1:
+                                            print("Its Polar Bear")
+                                        else:
+                                            print("Its a Penguine")
+#Wild Mamals :
+#Tree Ones :
+        else:
+            print("Does it stay in Trees??")
+            n=int(input())
+            if n==1:
+                print("It has 4 legs?")
+                n=int(input())
+                if n==1:
+                    print("Does it walk too slow?")
+                    n=int(input())
+                    if n==1:
+                        print("Its a Slauth")
+                    else:
+                        print("Does it humanlike like Monkey?")
+                        n=int(input())
+                        if n==1:
+                            print("Its a Chimpanzee or Orang-otang!!")
+                        else:
+                            print("Can it be seen near human living places and loves banana?")
+                            n=int(input())
+                            if n==1:
+                                print("Its a Monkey!")
+                            else:
+                                print("Does i eat bamboo?")
+                                n=int(input())
+                                if n==1:
+                                    print("Its a Panda!!")
+                                else:
+                                    print("Does it eat nuts?")
+                                    n=int(input())
+                                    if n==1:
+                                        print("Its a Squirrel!")
+                else:
+                    print("Is It colourfull?")
+                    n=int(input())
+                    if n==1:
+                        print("Its a wild Bird!!")
+#Land Mamals:
+            else:
+                print("Does It eat only plants??")
+                n=int(input())
+                if n==1:
+                    print("Have it long throat?")
+                    n=int(input())
+                    if n==1:
+                        print("Its a Giraffe!!")
+                    else:
+                        print("Has it white black cross colour??")
+                        n=int(input())
+                        if n==1:
+                            print("Its a Zebra!!")
+                        else:
+                            print("Is it too heavy animal?")
+                            n=int(input())
+                            if n==1:
+                                print("Has it a long trunk?")
+                                n=int(input())
+                                if n==1:
+                                    print("Its a Elephant!!")
+                                else:
+                                    print("Has is a sharp Nose??")
+                                    n=int(input())
+                                    if n==1:
+                                        print("Its a Rhinoceroses!!")
+                                    else:
+                                        print("Its a Hippopotamus!!")
+                            else:
+                                print("Does it walk by jumping??")
+                                n=int(input())
+                                if n==1:
+                                    print("Its a Kangaroo!!")
+                                else:
+                                    print("Its A Deer!!")
+                else:
+                    n=int(input("Does it eat only Meat?\n"))
+                    if n==1:
+                        n=int(input("Does it live in Ponds, Rivers and near land of ponds?\n"))
+                        if n==1:
+                            n=int(input("Are you talking about Snakes ?\n"))
+                            if n==1:
+                                n=int(input("Is it Poisonnous?\n"))
+                                if n==1:
+                                    n=int(input("Is it called King of Snakes ?"))
+                                    if n==1:
+                                        print("It is King Cobra !!")
+                                    else:
+                                        n=int(input("It Has Black Mouth ?\n"))
+                                        if n==1:
+                                            print("It is Black Mamba")
+                                else:
+                                    n=int(input("Is it very long and big?\n"))
+                                    if n==1:
+                                        print("It is Anaconda")
+                                    else:
+                                        print("It is Rat Snake family")
+                            else:
+                                n=int(input("Does it look like Big Lizard ?\n"))
+                                if n==1:
+                                    print("It is Comodo Dragon")
+                                else:
+                                    n=int(input("Is has Distorted Back with 4 legs and one tail?\n"))
+                                    if n==1:
+                                        print("It is Chrocodile !")
+                                    
+                                
+
+                        else:
+                            n=int(input("We call it king of Forest?\n"))
+                            if n==1:
+                                print("It is Lion !!")
+                            else:
+                                n=int(input("Is it run too fast?\n"))
+                                if n==1:
+                                    print("It is Cheetah")
+                                else:
+                                    n=int(input("Do it have Yellow white stripes?\n"))
+                                    if n==1:
+                                        print("It is Tiger")
+                                    else:
+                                        n=int(input("Does it stay in group and too agressive?\n"))
+                                        if n==1:
+                                            n=int(input("It looks like Husky Dog Breed ?\n"))
+                                            if n==1:
+                                                print("It is Wolf")
+                                            else:
+                                                print("It is Hyena")
+                                        else:
+                                            n=int(input("Doest it eat fish Basically ?\n"))
+                                            if n==1:
+                                                print("It is Bear")
+                                            else:
+                                                n=int(input("Its Colour is Black ?\n"))
+                                                if n==1:
+                                                    print("It is Black Jaguar !!")
+                                                
+                                            
+                                            
+                                    
+                                    
+                                    
+                                
+                                
+                                
+                                
+                            
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                
+                
+                    
+                        
+                
+                                  
+                                        
+                            
+                                
+               
+                        
+            
+                                        
+                                    
+                                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                
+                    
+            
+        
+                
+            
+        
+                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+                
+            
+
+
+
+
+
+
